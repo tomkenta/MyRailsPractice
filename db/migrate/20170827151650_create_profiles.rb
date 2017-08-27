459fa0ac2b067f6e47bfc9e5e654118afbad832f
tomkenta <tomkenta2014@gmail.com>
2017-08-28 00:28:30 +0900

    Add root

--- db/migrate/20170827151650_create_profiles.rb
+++ db/migrate/20170827151650_create_profiles.rb
@@ -2,7 +2,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
       t.string :name
-      t.int :age
+      t.integer :age
       t.string :address
 
       t.timestamps
